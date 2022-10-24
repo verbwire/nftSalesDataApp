1b04ee89ac5b32ab4529227c4241b6b080ee5073
0xbanks <116527130+0xbanks@users.noreply.github.com>
2022-10-24 19:55:09 -0400

    Add files via upload

---
+++ nftSalesData.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+// const sdk = require('api')('@verbwire/v1.0#hr2s143dl9hbr7s9');
+
+const API_KEY = 'sk_live_3c0c77fe-5b9e-4ec6-bdfd-f9698998d717';
+
+app.get('/', function(req, res){
+  const sdk = require('api')('@verbwire/v1.0#hr2s143dl9hbr7s9');
+
+sdk.auth(API_KEY);
+sdk.get('/nft/data/salesEventsForSlug', {
+  slug: 'boredapeyachtclub', // change slug to a project name of your choice
+  chain: 'ethereum',
+  limit: '25',     // change to adjust number of records returned
+  offset: '0',
+  sortDirection: 'DESC', //ascending and descending order...
+  startDate: '2022-10-01',// here you can change the dates (startningn date)
+  endDate: '2022-10-11'
+})
+  .then((data)=>{res.send(data)})
+  .catch(err => console.error(err));
+});
+
+app.listen(8080);
